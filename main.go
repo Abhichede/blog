6381abbb7b44ce3a9f0676ee60061b11e4196f77
Abhijit Chede <abhijit.chede@techverito.com>
2020-03-17 13:49:44 +0530

    initially adding all blog related files

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	posts "./posts/posts"
+)
+
+// Create a blog which displayes the blog posts from the json
+
+// to do that first we read the json file which contains the blogs
+
+func main() {
+	posts := posts.AllPosts()
+
+	for i := 0; i < len(posts.Posts); i++ {
+		fmt.Println("Title:", posts.Posts[i].Title)
+		fmt.Println("Description:", posts.Posts[i].Description)
+	}
+}
