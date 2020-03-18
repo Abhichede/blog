a7f3928a66727393ad0b7e53e81f41d4471fdad7
Abhijit Chede <abhijit.chede@techverito.com>
2020-03-18 15:37:34 +0530

    Adding the database intgration to create db add and select from table from pg

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"io/ioutil"
@@ -9,12 +10,28 @@ import (
 	"strconv"
 
 	"github.com/Abhichede/posts"
+	_ "github.com/lib/pq"
 )
 var _posts = posts.AllPosts()
+type Post struct {
+	Id int
+	Title, Description string
+}
+
+const (
+	host = "localhost"
+	port     = 5432
+  user     = "techverito_abhijit"
+  password = "postgres"
+  dbname   = "go_practice"
+)
 
 func main() {
 	http.Handle("/", http.HandlerFunc(LoadPosts))
 	http.Handle("/post", http.HandlerFunc(LoadPost))
+
+	// inserPost("Now second post", "This is now second post!!!")
+
 	fmt.Println("Server is listening on :8085 ")
 	err := http.ListenAndServe(":8085", nil)
 	if err != nil {
@@ -25,7 +42,7 @@ func main() {
 //LoadPosts ...
 func LoadPosts(w http.ResponseWriter, req *http.Request) {
 	var templ = template.Must(template.New("posts").Parse(readPosts()))
-	templ.Execute(w, _posts)
+	templ.Execute(w, getAllPosts())
 }
 
 //LoadPost ...
@@ -33,7 +50,7 @@ func LoadPost(w http.ResponseWriter, req *http.Request) {
 	var templ = template.Must(template.New("post").Parse(readPost()))
 	_ = templ
 	id, _ := strconv.Atoi(req.FormValue("id"))
-	templ.Execute(w, _posts.Posts[(id - 1)])
+	templ.Execute(w, getAllPosts()[id])
 }
 
 func readPosts() string {
@@ -43,4 +60,62 @@ func readPosts() string {
 func readPost() string {
 	postsTemplate, _ := ioutil.ReadFile("./templates/post.html")
 	return string(postsTemplate)
+}
+
+func getConnection() (db *sql.DB) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Successfully connected to postgres!")
+	return db
+}
+
+func inserPost(title, description string)  {
+	db := getConnection();
+	defer db.Close()
+	sqlStatement := `INSERT INTO posts(title, description) VALUES($1, $2)`;
+
+	_, err := db.Exec(sqlStatement, title, description)
+	checkErr(err)
+
+	fmt.Println("Post added!!!")
+}
+
+func getAllPosts() map[int]Post {
+	db := getConnection();
+	defer db.Close()
+	sqlStatement := `SELECT * FROM posts`;
+	posts__, err := db.Query(sqlStatement)
+	checkErr(err)
+	defer posts__.Close()
+	posts := make(map[int]Post)
+	i := 1
+	for posts__.Next() {
+		var post Post
+		err := posts__.Scan(&post.Id, &post.Title, &post.Description)
+		checkErr(err)
+		posts[i] = post
+		i++
+	}
+	return posts
+}
+
+func checkCount(rows *sql.Rows) (count int) {
+	count = 0
+ 	for rows.Next() {
+		count++
+  }
+  return count
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
